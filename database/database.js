ce08d08e78e33cbb54d9b4bad5edbce933b766ca
Prem Sangar <premsangar@kpmg.com>
2025-07-05 23:16:44 +0530

    database connection required changes completed

--- database/database.js
+++ database/database.js
@@ -1,11 +1,13 @@
 import pkg from 'pg';
 const { Client } = pkg;
+import dotenv from 'dotenv'; // ✅ use import if using ES modules
+dotenv.config();
 const client = new Client({
-    user: "dbuser",
-    host: "maglev.proxy.rlwy.net",
-    database: "railway",
-    password: "dbpass",
-    port: "36611",
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: parseInt(process.env.PG_PORT),
 });
 client.connect()
     .then(() => console.log('✅ Connected to PostgreSQL'))
