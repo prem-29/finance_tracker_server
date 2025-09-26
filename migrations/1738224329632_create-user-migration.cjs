86b62085301a3029a426db690417222f51508164
Prem Sangar <premsangar@kpmg.com>
2025-09-26 18:37:09 +0530

    migration issue fixed

--- migrations/1738224329632_create-user-migration.cjs
+++ migrations/1738224329632_create-user-migration.cjs
@@ -5,7 +5,18 @@ module.exports = {
             name: { type: 'varchar(100)', notNull: true },
             email: { type: 'varchar(100)', unique: true, notNull: true },
             password: { type: 'string', notNull: true },
-            created_at: { type: 'timestamp', default: pgm.func('current_timestamp') }
+            created_at: { type: 'timestamp', default: pgm.func('current_timestamp') },
+            is_verified: {
+                type: 'boolean',
+                notNull: true,
+                default: false,
+            },
+            is_deleted: {
+                type: 'boolean',
+                notNull: true,
+                default: false,
+            },
+            deleted_at: { type: 'timestamp' }
         });
     },
     down: async (pgm) => {
